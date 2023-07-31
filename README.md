8bce8f507d15360e5fb04c060344b9635b12e52f
Theodor-Alexandru Irimia <Theodor.Irimia@schuettflix.com>
2023-07-31 23:23:47 +0200

    feat: Implement a flychecker for Actionlint

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# flycheck-actionlint
-Flycheck integration for Actionlint
+# flycheck-actionlint - Flycheck for Github Actions
+Integration of [Actionlint](https://github.com/rhysd/actionlint) into [Flycheck](https://github.com/flycheck/flycheck).
+
+## Installation
+
+This package is not yet available on MELPA, so for now you either copy the [file](flycheck-actionlint.el) into your ```load-path``` or use [straight](https://github.com/radian-software/straight.el)
+
+```emacs-lisp
+(straight-use-package
+  '(flycheck-actionlint :type git :host github :repo "tirimia/flycheck-actionlint"))
+```
+
+Alternatively, if you are on a newer version of Emacs or already have use-package installed:
+
+```emacs-lisp
+(use-package flycheck-actionlint
+  :straight (:host github :repo "tirimia/flycheck-actionlint"))
+```
+
+## Usage
+
+Simply call the ```flycheck-actionlint-setup``` function. Be that in a hook or right after loading the package, up to you.
---
+++ flycheck-actionlint.el
@@ -0,0 +1,106 @@
+;;; flycheck-actionlint.el --- Flycheck integration for actionlint -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Theodor-Alexandru Irimia
+
+;; Author: Theodor-Alexandru Irimia
+;; Maintainer: Theodor-Alexandru Irimia
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26") (flycheck "32"))
+;; Homepage: https://github.com/tirimia/flycheck-actionlint
+;; Keywords: convenience, github, linter
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Flycheck integration for Actionlint. See README.md
+
+;;; Code:
+
+(require 'flycheck)
+
+(defgroup flycheck-actionlint nil
+  "Flycheck integration for Actionlint."
+  :prefix "flycheck-actionlint-"
+  :group 'flycheck)
+
+(defcustom flycheck-actionlint-shellcheck "shellcheck"
+  "Command name or file path of shellcheck external command. If nil, shellcheck integration will be disabled."
+  :type 'string
+  :group 'flycheck-actionlint)
+
+(defcustom flycheck-actionlint-pyflakes "pyflakes"
+  "Command name or file path of pyflakes external command. If empty, pyflakes integration will be disabled."
+  :type 'string
+  :group 'flycheck-actionlint)
+
+(defcustom flycheck-actionlint-ignore nil
+  "Regular expressions matching to error messages you want to ignore."
+  :type '(repeat string)
+  :group 'flycheck-actionlint)
+
+(defun flycheck-actionlint--project-root (&rest _)
+  "Return the nearest directory containing a .github folder."
+  (and buffer-file-name
+       (file-truename (locate-dominating-file buffer-file-name ".github"))))
+
+(defun flycheck-actionlint--enable-p ()
+  "Return non-nil if the actionlint linter should be enabled."
+  (and (flycheck-buffer-saved-p)
+       (string-match-p "\\.github/workflows/$"
+                       (file-name-directory (buffer-file-name)))
+       (string-match-p "\\.ya?ml$" (buffer-file-name))))
+
+(defun flycheck-actionlint--shellcheck-arg ()
+  "Generate the command line argument for shellcheck based on 'flycheck-actionlint-shellcheck'."
+  (format "-shellcheck=%s" flycheck-actionlint-shellcheck))
+
+(defun flycheck-actionlint--pyflakes-arg ()
+  "Generate the command line argument for pyflakes based on 'flycheck-actionlint-pyflakes'."
+  (format "-pyflakes=%s" flycheck-actionlint-pyflakes))
+
+(defun flycheck-actionlint--ignore-args ()
+  "Generate the command line arguments that the declared expressions in 'flycheck-actionlint-ignore' describe."
+  (let* ((formatter (lambda (exp) (format "-ignore '%s'" exp)))
+         (flags (mapcar formatter flycheck-actionlint-ignore))
+         (all-flags (string-join flags " ")))
+    (unless (string-empty-p all-flags) all-flags)))
+
+(flycheck-def-executable-var actionlint "actionlint")
+(flycheck-define-checker actionlint
+  "A Github Actions checker using actionlint"
+  :command ("actionline"
+            "-oneline"
+            (eval (flycheck-actionlint--shellcheck-arg))
+            (eval (flycheck-actionlint--pyflakes-arg))
+            (eval (flycheck-actionlint--ignore-args))
+            source-original)
+  :error-patterns ((error line-start (file-name) ":" line ":" column ": " (message) line-end))
+  :modes (yaml-mode yaml-ts-mode)
+  :predicate flycheck-actionlint--enable-p
+  :error-filter flycheck-increment-error-columns
+  :working-directory flycheck-actionlint--project-root)
+
+;;;###autoload
+(defun flycheck-actionlint-setup ()
+  "Setup Flycheck with Actionlint."
+  (interactive)
+  (add-to-list 'flycheck-checkers 'actionlint))
+
+(provide 'flycheck-actionlint)
+;;; flycheck-actionlint.el ends here
